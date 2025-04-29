9b85884fb72c4c4931b31d2a073d79ff3a33b195
mwndrly <mwndrly@gmail.com>
2025-04-29 11:12:17 -0300

    ref: create new classes

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class User {
-    private String name;
-    private String email;
-    private String password;
-
-    public User(String name, String email, String password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-}
-
-class Product {
-    private String name;
-    private double price;
-    private int quantity;
-
-    public Product(String name, double price, int quantity) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-    }
-
-    public String getName() { return name; }
-    public double getPrice() { return price; }
-    public int getQuantity() { return quantity; }
-}
-
 public class Main {
     private static ArrayList<User> users = new ArrayList<>();
     private static ArrayList<Product> products = new ArrayList<>();
